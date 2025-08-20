f058ad7243e85443963d561560d9ea702341658b
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-08-20 07:54:55 +0200

    Kontaktseite erstellt und mit Dummytext gefüllt

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Contact() {
+    return (
+        <>
+            <h2>Willkommen auf meiner Kontaktseite</h2>
+
+            <p>Fragen ? Ideen ? Anmerkungen? Dann schick  mir doch gerne eine Naricht. Per Mail oder Discord.</p>
+
+            {/*Hier noch Kontaktoption bzw. Kontaktformular ergänzen. */}
+        </>
+    )
+}
