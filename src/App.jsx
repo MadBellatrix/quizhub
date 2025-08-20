86d2749510686556925eb2bf3675acd89efc1801
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-08-20 07:53:52 +0200

    React Router integriert und Seitenstruktur mit Header, Footer, Homepage, Contact und Quiz angelegt

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+import Homepage from './pages/Homepage'
+import Contact from './pages/Contact'
+import Footer from './components/Footer'
+import Quiz from './components/Quiz/Quiz'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
+    
+      </Routes>
+      <Footer />
+    </Router>
+
+  )
+}
+
+export default App
