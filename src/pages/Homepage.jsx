321f22b2754c88805ec586e2bdac6158385095cd
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-08-21 09:32:10 +0200

    Modiauswahl hinzugefügt//Struktur angepasst

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,29 +4,65 @@ import { quizzes } from "../data";
 
 export default function Homepage() {
   const [selectedQuiz, setSelectedQuiz] = useState("math");
+  const [selectedMode, setSelectedMode] = useState("");
   const navigate = useNavigate();
 
+  
+  const modes = [
+    { id: "time", name: "Zeitmodus" },
+    { id: "practice", name: "Übungsmodus" },
+    { id: "score", name: "Highscore-Modus" }
+  ];
+
   return (
-    <div>
+    <section className="flex flex-col items-center justify-center p-5">
       <h1>Willkommen zu meiner Quiz-App</h1>
       <p>Wähle ein Quiz aus:</p>
 
-      {/* Dropdown: mit der Liste alle Quizzes */}
-      <select
-        value={selectedQuiz}
-        onChange={(e) => setSelectedQuiz(e.target.value)}
-      >
-        {Object.entries(quizzes).map(([id, quiz]) => (
-          <option key={id} value={id}>
-            {quiz.title}
-          </option>
-        ))}
-      </select>
+      <div>
+        {/* Dropdown: alle Quizzes */}
+        <select
+          value={selectedQuiz}
+          onChange={(e) => setSelectedQuiz(e.target.value)}
+        >
+          {Object.entries(quizzes).map(([id, quiz]) => (
+            <option key={id} value={id}>
+              {quiz.title}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <p>Wähle einen Modus:</p>
+        {/* Dropdown: Modi */}
+        <select
+          value={selectedMode}
+          onChange={(e) => setSelectedMode(e.target.value)}
+        >
+          <option value="">-- Bitte wählen --</option>
+          {modes.map((m) => (
+            <option key={m.id} value={m.id}>
+              {m.name}
+            </option>
+          ))}
+        </select>
+      </div>
 
       {/* Quiz wird erst nach Buttondruck gestartet */}
-      <button onClick={() => navigate(`/quiz/${selectedQuiz}`)}>
-        Starten
-      </button>
-    </div>
+      <div>
+        <button
+          onClick={() => {
+            if (!selectedMode) {
+              alert("Bitte zuerst einen Modus auswählen!");
+              return;
+            }
+            navigate(`/quiz/${selectedQuiz}/${selectedMode}/0`);
+          }}
+        >
+          Starten
+        </button>
+      </div>
+    </section>
   );
 }
