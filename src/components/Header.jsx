2cec3b8e97ecbee67e89578619768e6c884cef9c
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-08-21 10:04:19 +0200

    styling angepasst

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { faDungeon } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     return (
-        <header className='flex flex-row text-center justify-around mt-2 mb-7 border-b-2 border-gray-300 p-5'>
+        <header className='flex flex-row text-center justify-around mb-7 border-b-2 border-gray-300 p-5 bg-gray-800 text-white'>
             <FontAwesomeIcon icon={faDungeon} className="text-4xl text-green-600" />
             <h1 className='flex flex-col'>Willkommen auf meiner Quizpage</h1>
             <Nav />
