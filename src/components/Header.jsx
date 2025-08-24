b631aa612dfee5f59e5bc9cb60948dc2c94c7bd2
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-08-24 22:57:46 +0200

    Header und Navbar mit Styling versehen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,12 @@ import { faDungeon } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
     return (
-        <header className='flex flex-row text-center justify-around mb-7 border-b-2 border-gray-300 p-5 bg-gray-800 text-white'>
-            <FontAwesomeIcon icon={faDungeon} className="text-4xl text-green-600" />
-            <h1 className='flex flex-col'>Willkommen auf meiner Quizpage</h1>
+        <header className="relative w-full flex items-center justify-between px-8 py-6 bg-amber-900/95 shadow-md mb-12">
+                <FontAwesomeIcon icon={faDungeon} className="text-4xl text-lime-500 drop-shadow" />
+            <div className="flex items-center gap-4">
+                <h1 className="text-2xl text-center font-bold text-amber-100 tracking-wide ">Willkommen auf meiner Quizpage</h1>
+            </div>
             <Nav />
         </header>
-
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,31 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav>
-            <ul className='flex flex-row gap-4'>
+        <nav className=" bg-amber-100 rounded-xl shadow-md py-3 px-6 flex justify-end items-center">
+            <ul className="flex flex-row gap-4">
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link
+                        to="/"
+                        className="bg-amber-600 text-white px-4 py-2 rounded-lg font-medium shadow hover:bg-lime-700 transition-colors duration-200"
+                    >
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/contact">Kontakt</Link>
+                    <Link
+                        to="/highscores"
+                        className="bg-amber-600 text-white px-4 py-2 rounded-lg font-medium shadow hover:bg-lime-700 transition-colors duration-200"
+                    >
+                        Highscores
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/highscores">Highscores</Link>
+                    <Link
+                        to="/contact"
+                        className="bg-amber-600 text-white px-4 py-2 rounded-lg font-medium shadow hover:bg-lime-700 transition-colors duration-200"
+                    >
+                        Kontakt
+                    </Link>
                 </li>
             </ul>
         </nav>
