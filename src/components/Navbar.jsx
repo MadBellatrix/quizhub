c455f1b6a8cd301469019400c38fbae3cb973cfe
Michelle A. <loyalslytherindeatheater@gmail.com>
2025-08-25 09:59:02 +0200

    Styling der Nav angepasst für bessere lesbarkeit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className=" bg-amber-100 rounded-xl shadow-md py-3 px-6 flex justify-end items-center">
-            <ul className="flex flex-row gap-4">
+        <nav className="backdrop-blur-sm rounded-2xl shadow-lg py-4 px-8 flex justify-end items-center" >
+            <ul className="flex flex-row gap-2">
                 <li>
                     <Link
                         to="/"
-                        className="bg-amber-600 text-white px-4 py-2 rounded-lg font-medium shadow hover:bg-lime-700 transition-colors duration-200"
+                        className="bg-amber-100 text-amber-900 px-6 py-3 rounded-xl font-semibold shadow-md hover:bg-white hover:shadow-lg hover:scale-105 transition-all duration-300 border border-amber-200"
                     >
                         Home
                     </Link>
@@ -16,7 +16,7 @@ export default function Navbar() {
                 <li>
                     <Link
                         to="/highscores"
-                        className="bg-amber-600 text-white px-4 py-2 rounded-lg font-medium shadow hover:bg-lime-700 transition-colors duration-200"
+                        className="bg-amber-100 text-amber-900 px-6 py-3 rounded-xl font-semibold shadow-md hover:bg-white hover:shadow-lg hover:scale-105 transition-all duration-300 border border-amber-200"
                     >
                         Highscores
                     </Link>
@@ -24,7 +24,7 @@ export default function Navbar() {
                 <li>
                     <Link
                         to="/contact"
-                        className="bg-amber-600 text-white px-4 py-2 rounded-lg font-medium shadow hover:bg-lime-700 transition-colors duration-200"
+                        className="bg-amber-100 text-amber-900 px-6 py-3 rounded-xl font-semibold shadow-md hover:bg-white hover:shadow-lg hover:scale-105 transition-all duration-300 border border-amber-200"
                     >
                         Kontakt
                     </Link>
